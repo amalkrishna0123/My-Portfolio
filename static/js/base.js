5b9c9aa2c2323a85cfc3d800711c5f1b0078fbdb
amalkrishna0123 <amalikka0@gmail.com>
2023-10-14 16:17:52 +0530

    Initial commit

---
+++ static/js/base.js
@@ -0,0 +1,162 @@
+'use strict';
+
+
+
+/**
+ * PRELOAD
+ * 
+ * loading will be end after document is loaded
+ */
+
+const preloader = document.querySelector("[data-preaload]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+/**
+ * NAVBAR
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+
+
+/**
+ * HEADER & BACK TOP BTN
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+let lastScrollPos = 0;
+
+const hideHeader = function () {
+  const isScrollBottom = lastScrollPos < window.scrollY;
+  if (isScrollBottom) {
+    header.classList.add("hide");
+  } else {
+    header.classList.remove("hide");
+  }
+
+  lastScrollPos = window.scrollY;
+}
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 50) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+    hideHeader();
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+
+//TEXT ANIMATION EFFECT FOR HERO SECTION//
+ 
+
+const letterBoxes = document.querySelectorAll("[data-letter-effect]");
+
+let activeLetterBoxIndex = 0;
+let lastActiveLetterBoxIndex = 0;
+let totalLetterBoxDelay = 0;
+
+const setLetterEffect = function () {
+
+  // loop through all letter boxes
+  for (let i = 0; i < letterBoxes.length; i++) {
+    // set initial animation delay
+    let letterAnimationDelay = 0;
+
+    // get all character from the current letter box
+    const letters = letterBoxes[i].textContent.trim();
+    // remove all character from the current letter box
+    letterBoxes[i].textContent = "";
+
+    // loop through all letters
+    for (let j = 0; j < letters.length; j++) {
+
+      // create a span
+      const span = document.createElement("span");
+
+      // set animation delay on span
+      span.style.animationDelay = `${letterAnimationDelay}s`;
+
+      // set the "in" class on the span, if current letter box is active
+      // otherwise class is "out"
+      if (i === activeLetterBoxIndex) {
+        span.classList.add("in");
+      } else {
+        span.classList.add("out");
+      }
+
+      // pass current letter into span
+      span.textContent = letters[j];
+
+      // add space class on span, when current letter contain space
+      if (letters[j] === " ") span.classList.add("space");
+
+      // pass the span on current letter box
+      letterBoxes[i].appendChild(span);
+
+      // skip letterAnimationDelay when loop is in the last index
+      if (j >= letters.length - 1) break;
+      // otherwise update
+      letterAnimationDelay += 0.05;
+
+    }
+
+    // get total delay of active letter box
+    if (i === activeLetterBoxIndex) {
+      totalLetterBoxDelay = Number(letterAnimationDelay.toFixed(2));
+    }
+
+    // add active class on last active letter box
+    if (i === lastActiveLetterBoxIndex) {
+      letterBoxes[i].classList.add("active");
+    } else {
+      letterBoxes[i].classList.remove("active");
+    }
+
+  }
+
+  setTimeout(function () {
+    lastActiveLetterBoxIndex = activeLetterBoxIndex;
+
+    // update activeLetterBoxIndex based on total letter boxes
+    activeLetterBoxIndex >= letterBoxes.length - 1 ? activeLetterBoxIndex = 0 : activeLetterBoxIndex++;
+
+    setLetterEffect();
+  }, (totalLetterBoxDelay * 1000) + 3000);
+
+}
+
+// call the letter effect function after window loaded
+window.addEventListener("load", setLetterEffect);
